88eb00bed80892e27d6e1a16de2efbe04c7c7cc3
Martin <A>
2022-08-25 11:40:39 -0400

    Add comments for image_id.rs

--- src/image_id.rs
+++ src/image_id.rs
@@ -1,33 +1,48 @@
 use rand::seq::SliceRandom;
 use rocket::request::FromParam;
 use std::path::{Path, PathBuf};
+
+///The structure to manage ids of images.
 #[derive(Debug)]
 pub struct ImageId {
     id: String,
 }
+
 impl ImageId {
+    ///Build a new ImageId with an id size based on the *size* parameter.
     pub fn new(size: usize) -> Self {
+        //All the symbols to use
         const BASE62: &[u8] = b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
         let mut rng = rand::thread_rng();
+
         let id = (0..size)
             .into_iter()
             .map(|_| *BASE62.choose(&mut rng).unwrap())
             .collect::<Vec<_>>();
 
         let id = std::str::from_utf8(&id).unwrap().to_string();
+
         Self { id }
     }
+
+    ///Build a new ImageId from an existing id.
     pub fn from(id: &str) -> Self {
         Self { id: id.to_string() }
     }
+
+    ///Compute the path of the file.
     pub fn file_path(&self, root: &str) -> PathBuf {
         Path::new(root).join(&self.id)
     }
+
+    ///Return a reference to the id.
     pub fn get_id(&self) -> &str {
         &self.id
     }
 }
+
+///Allows to build an ImageId from a String, assuming this string is alphanumeric.
 impl<'a> FromParam<'a> for ImageId {
     type Error = &'a str;
 
