072b90e2f3608587a10d1d74d5eda1b6975f4ff0
Anger Jérémy <angerj.dev@gmail.com>
2022-10-02 21:48:42 +0200

    front: handle token and display resulting urls

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,4 +1,5 @@
-use web_sys::{Event, HtmlInputElement};
+use wasm_bindgen::JsValue;
+use web_sys::{Event, HtmlInputElement, UrlSearchParams};
 use yew::{html, html::TargetCast, Component, Context, Html};
 
 use gloo_file::File;
@@ -13,6 +14,45 @@ pub enum Msg {
 pub struct Model {
     files: Vec<File>,
     results: Vec<String>,
+}
+
+fn get_location_token() -> Option<String> {
+    let window = web_sys::window()?;
+    let search = window.location().search().ok()?;
+    UrlSearchParams::new_with_str(&search).ok()?.get("token")
+}
+
+fn get_url_of(upload_id: &str) -> Option<String> {
+    let window = web_sys::window()?;
+    let location = window.location();
+    let protocol = location.protocol().ok()?;
+    let host = location.host().ok()?;
+    Some(format!("{protocol}//{host}/user/get/{upload_id}"))
+}
+
+async fn upload_file(file: File, token: &str) -> Result<Msg, JsValue> {
+    let name = file.name();
+
+    let form = web_sys::FormData::new()?;
+    form.append_with_blob_and_filename("upload", file.as_ref(), &name)?;
+    form.append_with_str("title", &name)?;
+
+    let token = token.to_owned();
+    let res = Request::post(&format!("/user/post/{}", token))
+        .body(form)
+        .send()
+        .await;
+    if let Err(err) = res {
+        return Err(JsValue::from_str(&err.to_string()));
+    }
+
+    let res = res.unwrap().text().await;
+    if let Err(err) = res {
+        return Err(JsValue::from_str(&err.to_string()));
+    }
+
+    let res = res.unwrap();
+    Ok(Msg::Uploaded(res))
 }
 
 impl Component for Model {
@@ -35,53 +75,55 @@ impl Component for Model {
                 true
             }
             Msg::Upload => {
-                let token = "TODO";
-                for file in self.files.iter() {
-                    let form = web_sys::FormData::new().unwrap();
-                    let name = &file.name();
-                    form.append_with_blob_and_filename("upload", file.as_ref(), name)
-                        .unwrap();
-                    form.append_with_str("title", name).unwrap();
-                    ctx.link().send_future(async move {
-                        let res = Request::post(&format!("/user/post/{}", token))
-                            .body(form)
-                            .send()
-                            .await
-                            .unwrap()
-                            .text()
-                            .await
-                            .unwrap();
-                        Msg::Uploaded(res)
+                let token = get_location_token();
+                if let Some(token) = token {
+                    self.files.drain(..).for_each(|file| {
+                        let token = token.to_owned();
+                        ctx.link().send_future(async move {
+                            match upload_file(file, &token).await {
+                                Ok(msg) => msg,
+                                Err(_) => todo!(),
+                            }
+                        });
                     });
+                } else {
+                    let window = web_sys::window().unwrap();
+                    window
+                        .alert_with_message("Token not found. Use ?token=<your token> in the url.")
+                        .unwrap();
                 }
                 true
             }
             Msg::Uploaded(res) => {
-                self.results.push(res);
+                if let Some(url) = get_url_of(&res) {
+                    self.results.push(url);
+                }
                 true
             }
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
+        let on_click = |e: Event| {
+            let mut result = Vec::new();
+            let input: HtmlInputElement = e.target_unchecked_into();
+
+            if let Some(files) = input.files() {
+                let files = js_sys::try_iter(&files)
+                    .unwrap()
+                    .unwrap()
+                    .map(|v| web_sys::File::from(v.unwrap()))
+                    .map(File::from);
+                result.extend(files);
+            }
+            Msg::Files(result)
+        };
+
         html! {
             <div>
                 <div>
                     <p>{ "Choose files to upload" }</p>
-                    <input type="file" multiple=true onchange={ctx.link().callback(move |e: Event| {
-                            let mut result = Vec::new();
-                            let input: HtmlInputElement = e.target_unchecked_into();
-
-                            if let Some(files) = input.files() {
-                                let files = js_sys::try_iter(&files)
-                                    .unwrap()
-                                    .unwrap()
-                                    .map(|v| web_sys::File::from(v.unwrap()))
-                                    .map(File::from);
-                                result.extend(files);
-                            }
-                            Msg::Files(result)
-                        })}
+                    <input type="file" multiple=true onchange={ ctx.link().callback(on_click) }
                     />
                 </div>
                 <ul>
@@ -91,7 +133,7 @@ impl Component for Model {
                     <input value="Upload" type="button" onclick={ctx.link().callback(|_| Msg::Upload)} />
                 </div>
                 <div>
-                    { format!("{:?}", self.results) }
+                    { for self.results.iter().map(|url| Self::view_url(url.to_string())) }
                 </div>
             </div>
         }
@@ -104,7 +146,14 @@ impl Model {
         let mimetype = data.raw_mime_type();
         let size = data.size();
         html! {
-            <li>{ format!("{}: {}, {}", name, mimetype, size) }</li>
+            <li>{ format!("{}: {}, {}kb", name, mimetype, size / 1024) }</li>
+        }
+    }
+
+    fn view_url(url: String) -> Html {
+        let url2 = url.clone();
+        html! {
+            <p><a href={url}> {url2} </a></p>
         }
     }
 }
